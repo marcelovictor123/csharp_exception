9938235556fb0fe1ffb93cbef0417e0dffc90334
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-21 15:25:33 -0300

    Adicionando InnerException

---
+++ csharp_exception/OperacaoFinanceiraException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exception
+{
+    public class OperacaoFinanceiraException : Exception
+    {
+        public OperacaoFinanceiraException(string mensagem) : base(mensagem)
+        {
+
+        }
+        public OperacaoFinanceiraException(string mensagem, Exception excecaoInterna) : base(mensagem, excecaoInterna) {
+        
+        }
+    }
+}
--- csharp_exception/SaldoInsuficienteException.cs
+++ csharp_exception/SaldoInsuficienteException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace csharp_exception
 {
-    public class SaldoInsuficienteException : Exception
+    public class SaldoInsuficienteException : OperacaoFinanceiraException
     {
         public SaldoInsuficienteException(string mensagem) : base(mensagem) { 
         
