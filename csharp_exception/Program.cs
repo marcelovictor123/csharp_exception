1c56e326353b829703fdcbcac0c4404471a60be2
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-21 15:16:54 -0300

    Adicionando StackTrace

--- csharp_exception/Program.cs
+++ csharp_exception/Program.cs
@@ -4,17 +4,20 @@ using csharp_exception;
 
 try
 {
-    ContaCorrente conta1 = new ContaCorrente(278, "1234-X");
+    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+    /*
     conta1.Sacar(50);
     Console.WriteLine(conta1.GetSaldo());
 
     conta1.Sacar(150);
-    Console.WriteLine(conta1.GetSaldo());
+    Console.WriteLine(conta1.GetSaldo());*/
+
 }
 catch(ArgumentException ex)
 {
-    Console.WriteLine("Parâmetro com erro" + ex.ParamName);
+    Console.WriteLine("Parâmetro com erro " + ex.ParamName);
     Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+    Console.WriteLine(ex.StackTrace);
     Console.WriteLine(ex.Message);
     
 }
